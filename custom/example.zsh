4b85c772e991e8e02a1691567789d5db1cf3cbb4
Jonathan Baird <jonathan@snappworks.com>
2017-03-20 12:08:41 -0600

    make the last of the changes available

--- custom/example.zsh
+++ custom/example.zsh
@@ -3,3 +3,4 @@
 #
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=251"        # This styles the zsh-autosuggestion plugin

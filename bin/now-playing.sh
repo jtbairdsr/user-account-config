0dfa349f47f5ce444466b682ee3f1a0c2031a571
Jonathan Baird <jtbairdj@gmail.com>
2018-10-07 12:35:41 -0600

    update tmux stuff

--- bin/now-playing.sh
+++ bin/now-playing.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-osascript -e 'tell application "iTunes" to if player state is playing then name of current track & " - " & artist of current track'
+osascript -e 'tell application "iTunes" to if player state is playing then name of current track & " - " & artist of current track' | cut -d ' ' -f 1-6

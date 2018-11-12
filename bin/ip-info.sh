c602ebfd1b1b58d4edc96815f1474cdd4ca52f5d
Jonathan Baird <jtbairdj@gmail.com>
2018-11-12 13:21:03 -0700

    misc updates

--- bin/ip-info.sh
+++ bin/ip-info.sh
@@ -6,14 +6,17 @@ PUB_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
 echo -n "$IP  $PUB_IP"
 
-# INTERNET=""
+INTERNET=" "
 
-# internet_info=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep agrCtlRSSI | awk '{print $2}' | sed 's/-//g')
-#
-# if [[ "$internet_info" -lt 20 ]]; then
-#     echo -n '#[fg=red]'
-# elif [[ "$internet_info" -lt 30 ]]; then
-#     echo -n '#[fg=yellow]'
-# else
-#     echo -n '#[fg=green]'
-# fi
+internet_info=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep agrCtlRSSI | awk '{print $2}' | sed 's/-//g')
+
+if [[ "$internet_info" -lt 20 ]]; then
+    echo -n '#[fg=red]'
+    echo "$INTERNET"
+elif [[ "$internet_info" -lt 30 ]]; then
+    echo -n '#[fg=yellow]'
+    echo "$INTERNET"
+else
+    echo -n '#[fg=green]'
+    echo "$INTERNET"
+fi
